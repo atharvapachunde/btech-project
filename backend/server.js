567f8e256c9fe8fb3bf0f9a41c969d7190305d5f
atharvapachunde <atharvapachunde@gmail.com>
2025-08-08 16:51:50 +0530

    first api

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get("/api/message", (req,res) => {
+    res.send("your message has been sent successfully");
+})
+
 app.listen(8080, () => {
     console.log("Server started on PORT: 8080"); 
 })
